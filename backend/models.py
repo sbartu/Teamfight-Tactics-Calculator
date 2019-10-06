9439381243ce3308dcf6ffbca07c22fb3c15fd51
Bartu Sivri <sbartu@umich.edu>
2019-10-06 07:29:44 -0400

    Setting up database.

--- backend/models.py
+++ backend/models.py
@@ -2,9 +2,16 @@ from django.db import models
 
 
 class Champion(models.Model):
+    name = models.CharField(max_length=500)
+    imagefile = models.FileField(upload_to='images/', null=True, verbose_name="")
 
+    def __str__(self):
+        return self.name + ": " + str(self.imagefile)
 
 class Item(models.Model):
+    name = models.CharField(max_length=500)
+    imagefile = models.FileField(upload_to='images/', null=True, verbose_name="")
 
+    def __str__(self):
+        return self.name + ": " + str(self.imagefile)
 
-class TFTChampion(models.Model):
